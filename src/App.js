26324c3c24080ccd12c71e7189bc209a681f70ff
Sarthak Patel <sarthakpatel231@gmail.com>
2023-07-04 20:04:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import { Button, ButtonGroup } from '@chakra-ui/react'
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+ import Header from './components/Header';
+function App() {
+  return (
+     
+     <Router> 
+       <Header />
+   
+      <Routes>
+         
+        <Route/>
+      </Routes>
+     </Router>
+  );
+}
+
+export default App;
