743a35c7e6f0944d5862c9190f0688f6995cca73
Sarthak Patel <sarthakpatel231@gmail.com>
2023-07-06 17:02:24 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 //import { Button, ButtonGroup } from '@chakra-ui/react'
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
  import Header from './components/Header';
-   
+  import Footer from './components/Footer ';
   import Home from './components/Home'
 function App() {
   return (
      
      <Router> 
        <Header />
-   
+       
       <Routes>
          
         <Route path="/"element={<Home/>} />
       </Routes>
+      <Footer/>
      </Router>
   );
 }
